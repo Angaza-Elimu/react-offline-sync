f571b8cea1d2a75dfb8f62a036f40f29ec7bb099
Chiama <shawn.chiama@strathmore.edu>
2021-02-03 01:13:31 +0300

    Ignore .jest folder

--- .jest/cache/ts-jest/95/7321ce33d840b09fc0a43d2576c244607b8f68/c86f99d52634b4ac96ee3e5f676134754e154c72.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import { ExampleComponent } from '.'
-// describe('ExampleComponent', () => {
-//   it('is truthy', () => {
-//     expect(ExampleComponent).toBeTruthy()
-//   })
-// })
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJmaWxlIjoiL1VzZXJzL0RldkFDL0Rlc2t0b3AvQW5nYXphL3JlYWN0LW9mZmxpbmUtc3luYy9zcmMvaW5kZXgudGVzdC50c3giLCJtYXBwaW5ncyI6IjtBQUFBLHVDQUF1QztBQUV2Qyx1Q0FBdUM7QUFDdkMsNEJBQTRCO0FBQzVCLDRDQUE0QztBQUM1QyxPQUFPO0FBQ1AsS0FBSyIsIm5hbWVzIjpbXSwic291cmNlcyI6WyIvVXNlcnMvRGV2QUMvRGVza3RvcC9BbmdhemEvcmVhY3Qtb2ZmbGluZS1zeW5jL3NyYy9pbmRleC50ZXN0LnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBpbXBvcnQgeyBFeGFtcGxlQ29tcG9uZW50IH0gZnJvbSAnLidcblxuLy8gZGVzY3JpYmUoJ0V4YW1wbGVDb21wb25lbnQnLCAoKSA9PiB7XG4vLyAgIGl0KCdpcyB0cnV0aHknLCAoKSA9PiB7XG4vLyAgICAgZXhwZWN0KEV4YW1wbGVDb21wb25lbnQpLnRvQmVUcnV0aHkoKVxuLy8gICB9KVxuLy8gfSlcbiJdLCJ2ZXJzaW9uIjozfQ==
