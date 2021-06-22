3ba75cd175445de3b9fad0e34273cfc0b68cff3f
Chiama <shawn.chiama@strathmore.edu>
2021-06-22 20:22:20 +0300

    More tests

---
+++ src/test/NetworkConsumer.test.ts
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react';
+import { Text } from 'react-native';
+import { render } from 'react-native-testing-library';
+import NetworkConsumer from '../components/NetworkConsumer';
+import NetworkProvider from '../components/NetworkProvider';
+
+const getElement = {
+
+};
+
+function Consumer() {
+ 
+}
+
+describe.only('NetworkConsumer', () => {
+  it(`throws if it's not rendered within the Provider`, () => {
+   
+  });
+
+  it('receives isConnected prop from Provider using context', () => {
+   
+  });
+});
